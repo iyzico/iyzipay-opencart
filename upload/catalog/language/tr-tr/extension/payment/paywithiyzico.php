ab566080e5fca39532ed129101ab0e6d5e293fb2
said.ozgat <said.ozgat@hotmail.com>
2021-06-24 11:06:21 +0300

    +pwi svg logo update

--- upload/catalog/language/tr-tr/extension/payment/paywithiyzico.php
+++ upload/catalog/language/tr-tr/extension/payment/paywithiyzico.php
@@ -1,7 +1,7 @@
 <?php
 // Text
 $_['pwi_title'] = 'iyzico ile Öde';
-$_['pwi_img_title'] 			= '<img style="width: 30%" src="admin/view/image/payment/paywithiyzico_logo_tr.png"/>';
+$_['pwi_img_title'] 			= '<img style="width: 20%" src="admin/view/image/payment/pay-with-iyzico-tr.svg"/>';
 $_['payment_failed'] 		  	= 'Ödeme Başarısız';
 $_['payment_field_desc'] 	 	= 'Ödeme Numarası: ';
 $_['installement_field_desc'] 	= ' Taksit ';
