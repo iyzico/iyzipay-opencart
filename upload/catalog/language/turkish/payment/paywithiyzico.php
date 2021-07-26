59e5fe59f152037a06929c2c4fc951c5b34bf821
said.ozgat <said.ozgat@hotmail.com>
2021-07-26 17:28:51 +0300

    +deleted all files

--- upload/catalog/language/turkish/payment/paywithiyzico.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Text
-$_['pwi_title'] = 'iyzico ile Öde';
-$_['pwi_img_title'] 			= '<img style="width: 20%" src="admin/view/image/payment/pay-with-iyzico-tr.svg"/>';
-$_['payment_failed'] 		 	= 'Ödeme Başırısız';
-$_['payment_field_desc'] 	 	= 'Ödeme Numarası: ';
-$_['installement_field_desc'] 	= 'Taksit Vade Komisyonu';
-$_['installement_count']        = ' Taksit';
--- upload/catalog/view/theme/default/template/payment/paywithiyzico_form.tpl
+++
@@ -1,4 +0,0 @@
-<meta http-equiv="Refresh" content="0; url='<?php echo $pwi_redirect; ?>'" />
-<script type="text/javascript">
-    window.location.href = "<?php echo $pwi_redirect; ?>";
-</script>
