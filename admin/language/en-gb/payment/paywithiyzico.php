7dc7f6907e534486da56f5379ae2e42f8f365b8a
said.ozgat <said.ozgat@hotmail.com>
2021-07-26 17:30:19 +0300

    +opencart 2.x pwi

---
+++ admin/language/en-gb/payment/paywithiyzico.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']    = 'Pay with iyzico';
+
+// Text
+$_['text_module']      = 'Payment';
+$_['text_paywithiyzico'] = '<a href="https://www.iyzico.com/isim-icin/iyzico-ile-ode" target="_blank"><img width="30%" src="view/image/payment/pay-with-iyzico.svg" alt="iyzico" title="iyzico"/></a>';
+$_['extension_status'] = 'Extension Status';
+$_['text_enabled'] = 'Enabled';
+$_['text_disabled'] = 'Disabled';
+
+// Order Status
+$_['entry_order_status'] = 'Order Status';
+$_['entry_status'] = 'Status';
+$_['entry_cancel_order_status'] = "Cancel Order Status";
+$_['order_status_after_payment_tooltip'] = 'Order status after successful payment';
+$_['order_status_after_cancel_tooltip'] = 'Order status after successful cancel';
+$_['entry_sort_order'] = 'Sort Order';
+
+// Error
+$_['error_permission'] = 'You are not authorized!';
+$_['error_status'] = 'Invalid Status';
+$_['error_order_status_id'] = 'Invalid Order Status!';
+$_['error_cancel_order_status_id'] = 'Invalid Cancel Order Status!';
---
+++ admin/language/turkish/payment/paywithiyzico.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']    = 'iyzico ile Öde';
+
+// Text
+$_['text_module']      = 'Ödeme';
+$_['text_paywithiyzico'] = '<a href="https://www.iyzico.com/isim-icin/iyzico-ile-ode" target="_blank"><img width="30%" src="view/image/payment/pay-with-iyzico-tr.svg" alt="iyzico" title="iyzico"/></a>';
+$_['extension_status'] = 'Eklenti Durumu';
+$_['text_enabled'] = 'Açık';
+$_['text_disabled'] = 'Kapalı';
+
+// Order
+$_['entry_order_status'] = 'Sipariş Durumu';
+$_['entry_status'] = 'Status';
+$_['entry_cancel_order_status'] = "İptal Durumu";
+$_['order_status_after_payment_tooltip'] = 'Başarılı ödeme sonrasında müşteriye gösterilecek sipariş durum bilgisi';
+$_['order_status_after_cancel_tooltip'] = 'Sipariş iptal edildiğinde müşteriye gösterilecek sipariş durum bilgisi';
+$_['entry_sort_order'] = 'Sıralama';
+
+// Error
+$_['error_permission'] = 'Yetkiniz yok!';
+$_['error_status'] = 'Geçersiz Eklenti Durumu';
+$_['error_order_status_id'] = 'Geçersiz Sipariş Durumu!';
+$_['error_cancel_order_status_id'] = 'Geçersiz İptal Durumu!';
