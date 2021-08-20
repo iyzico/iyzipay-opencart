d53f2ea74a03282f3efbb1d5e0fffda59e8476c1
said.ozgat <said.ozgat@hotmail.com>
2021-08-20 12:58:52 +0300

    +delete all files

--- upload/catalog/language/tr-tr/extension/payment/iyzico.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Text
-$_['payment_failed'] 		  	= 'Ödeme Başarısız';
-$_['text_title'] 	 		  	= 'iyzico';
-$_['payment_field_desc'] 	 	= 'Ödeme Numarası: ';
-$_['installement_field_desc'] 	= ' Taksit ';
