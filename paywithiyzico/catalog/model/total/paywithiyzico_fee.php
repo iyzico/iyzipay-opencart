fb26c834b3b516f499b7a1c3e7f6525e77bbbc1d
feyzullah Demir <101548542+feyzullahdemir@users.noreply.github.com>
2023-04-18 09:54:20 +0300

    +new plugin

---
+++ paywithiyzico/catalog/model/total/paywithiyzico_fee.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Opencart\Catalog\Model\Extension\iyzico\Total;
+class paywithiyzico extends \Opencart\System\Engine\Model{
+
+    public function confirm($order_info, $order_total){
+
+    }
+
+}
